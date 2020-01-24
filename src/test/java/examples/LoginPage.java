0eecce4811191ff7078ab9a23d82db05a6cf1f41
LukaszBola <lukasz.bola@opi.org.pl>
2020-01-24 16:22:50 +0100

    Refactor of project using PageFactory class and @FindBy annotation.

--- src/test/java/examples/LoginPage.java
+++ src/test/java/examples/LoginPage.java
@@ -3,33 +3,45 @@ package examples;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
+
+    @FindBy(css = "#Catalog input[name*='username']")
+    private WebElement usernameField;
+
+    @FindBy(css = "#Catalog input[name*='password']")
+    private WebElement passwordField;
+
+    @FindBy(css = "#Catalog input[name*='signon']")
+    private WebElement loginButton;
+
+    @FindBy(css = "#Content li")
+    private WebElement warningMessage;
+
     private WebDriver driver;
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public void typeIntoUsernameField(String username) {
-        WebElement usernameField = driver.findElement(By.cssSelector("#Catalog input[name*='username']"));
         usernameField.clear();
         usernameField.sendKeys(username);
     }
 
     public void typeIntoPasswordField(String password) {
-        WebElement passwordField = driver.findElement(By.cssSelector("#Catalog input[name*='password']"));
         passwordField.clear();
         passwordField.sendKeys(password);
     }
 
     public void clickLoginButton() {
-        WebElement loginButton = driver.findElement(By.cssSelector("#Catalog input[name*='signon']"));
         loginButton.click();
     }
 
-    public String getWarningMessage(){
-        String warningMessage = driver.findElement(By.cssSelector("#Content li")).getText();
-        return warningMessage;
+    public String getWarningMessage() {
+        return warningMessage.getText();
     }
 }
--- src/test/java/examples/TopMenuPage.java
+++ src/test/java/examples/TopMenuPage.java
@@ -3,16 +3,22 @@ package examples;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class TopMenuPage {
+
+    @FindBy (css = "#MenuContent a[href*='signonForm']")
+    WebElement signInLink;
+
     private WebDriver driver;
 
     public TopMenuPage(WebDriver driver){
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public void clickOnSignInLink() {
-        WebElement signInLink = driver.findElement(By.cssSelector("#MenuContent a[href*='signonForm']"));
         signInLink.click();
     }
 }
